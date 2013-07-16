ae19b50745f20bd3537bb28c9c7f9bb4d36c524d
Andy Hamilton <andyhmltn@gmail.com>
2013-07-16 12:56:17 +0100

    The script is now working correctly. [v0.1.0]

---
+++ pyscp.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
+########################
+#
+# Configuration
+#
+########################
+HOME   = os.path.expanduser("~")+'/'
+CONFIG = {
+  'path'        : HOME+'.pyscp/',
+  'config_file' : 'config.json',
+  'template':{
+    'directory_mapping':{
+      'all':{}
+    }
+  }
+}
+
+########################
+#
+# Version function
+#
+########################
+def fetch_version(return_version_string=True):
+  VERSION = {
+    'MAJOR':0,
+    'MINOR':1,
+    'PATCH':0
+  }
+
+  if return_version_string:
+    return "PySCP Version: " + str(VERSION['MAJOR'])+'.'+str(VERSION['MINOR'])+'.'+str(VERSION['PATCH'])
+  else:
+    return VERSION
+
+########################
+#
+# Init Script
+#
+########################
+def install_pyscp(CONFIG):
+  if not os.path.exists(CONFIG['path']):
+    os.makedirs(CONFIG['path'])
+
+    with open(CONFIG['path']+CONFIG['config_file'], 'w+') as outfile:
+      json.dump(CONFIG['template'], outfile)
+
+    print "PySCP has been installed succesfully"
+  else:
+    print "PySCP is already installed! Edit ~/.pyscp/config.json to begin"
+
+########################
+#
+# Main PySCP Script
+#
+########################
+def run_pyscp(arguments):
+  from_path = arguments[1]
+  to_path   = arguments[2]
+
+  from_directory = os.path.dirname(from_path)
+
+  directory_mapping_file = open(CONFIG['path']+CONFIG['config_file'])
+  directory_mapping      = json.load(directory_mapping_file)
+
+  remote_host_array = to_path.split("@")
+
+  remote_host = remote_host_array[0] if len(remote_host_array) == 1 else remote_host_array[1]
+
+  if from_directory in directory_mapping['directory_mapping']['all']:
+    os.system(format_scp_string(from_path, to_path, directory_mapping['directory_mapping']['all'][from_directory]))
+  else:
+    if remote_host in directory_mapping['directory_mapping']:
+      os.system(format_scp_string(from_path, to_path, directory_mapping['directory_mapping'][remote_host][from_directory]))
+    else:
+      print "There are no rules setup for that host"
+
+def format_scp_string(from_path,to_path,replace):
+  return "scp " + from_path + " " + to_path + ":" + replace
+
+########################
+#
+# Handle PySCP Arguments
+#
+########################
+if len(sys.argv) > 1:
+
+  if sys.argv[1] == '--init':
+    install_pyscp(CONFIG)  
+
+  elif sys.argv[1] == '-v':
+    print fetch_version()
+
+  if len(sys.argv) > 2:
+    run_pyscp(sys.argv)
+
